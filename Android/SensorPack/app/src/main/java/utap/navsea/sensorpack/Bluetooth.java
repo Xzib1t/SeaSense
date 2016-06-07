effa3ec192465e399dec91f50c3a4f4a52960b0f
Joseph Maestri <jmaestri123@gmail.com>
2016-06-07 11:20:48 -0400

    Added ability to separate incoming CSV data and print to screen

--- Android/SensorPack/app/src/main/java/utap/navsea/sensorpack/Bluetooth.java
+++ Android/SensorPack/app/src/main/java/utap/navsea/sensorpack/Bluetooth.java
@@ -29,6 +29,7 @@ import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -44,6 +45,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.UUID;
 
@@ -55,7 +58,23 @@ public class Bluetooth extends AppCompatActivity{
     private static final UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private static BluetoothDevice device = null;
     private static BluetoothAdapter mBluetoothAdapter;
+
     StringBuilder total = new StringBuilder();
+
+
+    private ArrayList<Float> temperature = new ArrayList<Float>();
+    private ArrayList<Float> depth = new ArrayList<Float>();
+    private ArrayList<Float> conductivity = new ArrayList<Float>();
+    private ArrayList<Float> light = new ArrayList<Float>();
+    private ArrayList<Float> heading = new ArrayList<Float>();
+    private ArrayList<Float> accelX = new ArrayList<Float>();
+    private ArrayList<Float> accelY = new ArrayList<Float>();
+    private ArrayList<Float> accelZ = new ArrayList<Float>();
+    private ArrayList<Float> gyroX = new ArrayList<Float>();
+    private ArrayList<Float> gyroY = new ArrayList<Float>();
+    private ArrayList<Float> gyroZ = new ArrayList<Float>();
+    private String downloadedStrings = new String();
+    private ArrayList<String> downloadedData = new ArrayList<String>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -120,20 +139,51 @@ public class Bluetooth extends AppCompatActivity{
      */
     private void readData(InputStream inStream) {
     try {
-
-        byte[] buffer = new byte[256];  // buffer store for the stream
-        int bytes; // bytes returned from read()
-
         DataInputStream mmInStream = new DataInputStream(inStream);
 
         // Read from the InputStream
-        bytes = inStream.read(buffer);
-        String readMessage = new String(buffer, 0, bytes);
-        for(int i=0; i<100; i++)  print2BT(readMessage);
 
-    } catch (Exception e) {
-        //TODO
-    }
+
+            boolean delimFound = false;
+
+            while (!delimFound) {
+
+                byte[] buffer = new byte[256];  // buffer store for the stream
+                int bytes; // bytes returned from read()
+                bytes = inStream.read(buffer);
+                String readMessage = new String(buffer, 0, bytes);
+
+                downloadedData.add(readMessage);
+
+                boolean check = check4eof(downloadedData);
+
+                if (check) {
+                    for (String printStr : downloadedData) {
+                        downloadedStrings = downloadedStrings.concat(printStr);
+                    }
+                    //print2BT(downloadedStrings);
+                    parseData(downloadedStrings);
+                    //graphTest(chart1, convert2Entry(temperature), "Temperature data", Color.RED);
+                    //graphTest(chart2, convert2Entry(light), "Light data", Color.GREEN);
+
+                    print2BT("Temperature: " + temperature.toString() + "\n");
+                    print2BT("Depth: " + depth.toString() + "\n");
+                    print2BT("Conductivity: " + conductivity.toString() + "\n");
+                    print2BT("Light: " + light.toString() + "\n");
+                    print2BT("Heading: " + heading.toString() + "\n");
+                    print2BT("Accelerometer X: " + accelX.toString() + "\n");
+                    print2BT("Accelerometer Y: " + accelY.toString() + "\n");
+                    print2BT("Accelerometer Z: " + accelZ.toString() + "\n");
+                    print2BT("Gyroscope X: " + gyroX.toString() + "\n");
+                    print2BT("Gyroscope Y: " + gyroY.toString() + "\n");
+                    print2BT("Gyroscope Z: " + gyroZ.toString());
+
+                    delimFound = true;
+                }
+            }
+        }catch(Exception e){
+            //TODO
+        }
 }
 
     private void connect2device(BluetoothDevice mBluetoothAdapter) {
@@ -166,6 +216,8 @@ public class Bluetooth extends AppCompatActivity{
         });
     }
 
+
+
     private void writeData(OutputStream outStream){
 
        try {
@@ -173,15 +225,107 @@ public class Bluetooth extends AppCompatActivity{
            mmOutStream.write(108); //This is "logapp" in ASCII code, it is the command
            mmOutStream.write(111); //that causes the Arduino side to start sending data
            mmOutStream.write(103);
-           mmOutStream.write(0);
            mmOutStream.write(97);
            mmOutStream.write(112);
            mmOutStream.write(112);
-           mmOutStream.write(13);
+           mmOutStream.write(13); //carriage return
        }
        catch(IOException e){
            //TODO
        }
+    }
+
+    private boolean check4eof(ArrayList<String> inputString){
+        String input = inputString.get(inputString.size() - 1); //Read last entry in array list
+        char[] eof = {'U','+','1','F','4','A','9'};
+        int eofLength = 7;
+        char[] charCheck = new char[eofLength];
+        if(input.length() >= eofLength){
+            int iterate = 0;
+            for(int i=input.length()-eofLength; i<input.length(); i++){
+                charCheck[iterate] = input.charAt(i);
+                iterate++;
+            }
+
+            if(Arrays.equals(charCheck,eof)) return true;
+        }
+        return false;
+    }
+
+    private void parseData(String input){
+        int dataType = 0;
+        int curIndex = 0;
+        String eof = "U+1F4A9";//" U+1F4A9";
+        ArrayList<String> parsedData = new ArrayList<String>();
+
+        for(String splitVal : input.split(",")){
+            parsedData.add(splitVal);
+            switch(dataType){
+                case 0:
+                    if( !(eof.equals( parsedData.get(curIndex) ) ) ){ //make sure we don't use the eof
+                        temperature.add(Float.parseFloat(parsedData.get(curIndex)));
+                    }
+                    break;
+                case 1:
+                    if( !(eof.equals( parsedData.get(curIndex) ) ) ){ //make sure we don't use the eof
+                        depth.add(Float.parseFloat(parsedData.get(curIndex)));
+                    }
+                    break;
+                case 2:
+                    if( !(eof.equals( parsedData.get(curIndex) ) ) ){ //make sure we don't use the eof
+                        conductivity.add(Float.parseFloat(parsedData.get(curIndex)));
+                    }
+                    break;
+                case 3:
+                    if( !(eof.equals( parsedData.get(curIndex) ) ) ){ //make sure we don't use the eof
+                        light.add(Float.parseFloat(parsedData.get(curIndex)));
+                    }
+
+                    break;
+                case 4:
+                    if( !(eof.equals( parsedData.get(curIndex) ) ) ){ //make sure we don't use the eof
+                        heading.add(Float.parseFloat(parsedData.get(curIndex)));
+                        //spinCompass(compass, Float.parseFloat(parsedData.get(curIndex)));
+                    }
+                    break;
+                case 5:
+                    if( !(eof.equals( parsedData.get(curIndex) ) ) ){ //make sure we don't use the eof
+                        accelX.add(Float.parseFloat(parsedData.get(curIndex)));
+                    }
+                    break;
+                case 6:
+                    if( !(eof.equals( parsedData.get(curIndex) ) ) ){ //make sure we don't use the eof
+                        accelY.add(Float.parseFloat(parsedData.get(curIndex)));
+                    }
+                    break;
+                case 7:
+                    if( !(eof.equals( parsedData.get(curIndex) ) ) ){ //make sure we don't use the eof
+                        accelZ.add(Float.parseFloat(parsedData.get(curIndex)));
+                    }
+                    break;
+                case 8:
+                    if( !(eof.equals( parsedData.get(curIndex) ) ) ){ //make sure we don't use the eof
+                        gyroX.add(Float.parseFloat(parsedData.get(curIndex)));
+                    }
+                    break;
+                case 9:
+                    if( !(eof.equals( parsedData.get(curIndex) ) ) ){ //make sure we don't use the eof
+                        gyroY.add(Float.parseFloat(parsedData.get(curIndex)));
+                    }
+                    break;
+                case 10:
+                    if( !(eof.equals( parsedData.get(curIndex) ) ) ){ //make sure we don't use the eof
+                        gyroZ.add(Float.parseFloat(parsedData.get(curIndex)));
+                    }
+                    break;
+                default:
+
+                    break;
+            }
+            curIndex++;
+            dataType++;
+            if(dataType>10) dataType = 0; //reset data counter
+        }
     }
 
     private void print2BT(String theString){
