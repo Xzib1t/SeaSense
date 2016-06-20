c9423f227dc9d4a977bd105cf412ebbe8ea06298
Georges Gauthier <glgauthier@wpi.edu>
2016-06-20 08:10:28 -0400

    Better Data Management, Statistics option!
    - Got rid of some for() loops and replaced them with map functions
    - Added a statistics mode, ability to toggle stats based on what lines
    are selected
    - Ability to show/hide a line by default

--- js/parse.js
+++ js/parse.js
@@ -11,8 +11,9 @@ var chart; // d3 graph - global so it can be updated otf
 
 var plottedData = []; // data currently being plotted
 var timeStamps = []; // timestamps matching the current data set(s)
-var stamp,xMin,xMax; // year/month/day, first and last timestamps
-
+var stamp; // year/month/day
+var min,max; // brush extent (used in calculating averages)
+var stats = 0;
 var parser = d3.time.format.utc("%Y/%m/%d %H:%M:%S"); // convert timestamps to d3 time format
 
 // data interpolation methods
@@ -22,10 +23,11 @@ var methodNum = 4; // current interpolation method (cardinal by default)
 var filename = "example.csv"; // current filename (example by default)
 
 // object constructor function used in creating d3-graphable datasets
-function dataSet(area,key,values) {
+function dataSet(area,key,values,disabled) {
   this.area = area;
   this.key = key;
   this.values = values;
+  this.disabled = disabled;
 }
 
 // object constructor function used in creating individual datasets
@@ -37,6 +39,7 @@ function value(x,y) {
 // event listeners
 $(document).ready(function(){
     // run when the page is opened
+     $("#avg-data").hide();
     $("#chartObj").text(methods[methodNum]);
     $("#filename").text(filename);
     //run based on user input
@@ -44,6 +47,7 @@ $(document).ready(function(){
     $("#newFile").click(showFileUI);
     $("#example").click(plotExampleData);
     $("#interp").click(interpolateData);
+    $("#stats").click(showStats);
 });
 
 // used to reload the page for a new file input
@@ -52,9 +56,31 @@ function showFileUI(){
     location.reload(); // reload the page 
 }
 
+function showStats(){
+    stats = !stats;
+    if(stats){
+        document.getElementById("chart").style.width = "calc(100% - 150px)";
+        $("#avg-data").show(500);
+        $( "#statistics" ).empty();
+        $( "#statistics" ).append("on");
+        
+    }
+    else{
+       document.getElementById("chart").style.width = "100%"; 
+        $("#avg-data").hide(500);
+        $( "#statistics" ).empty();
+        $( "#statistics" ).append("off");
+    }
+    d3.select('#chart svg')
+            .datum(plottedData)
+            .call(chart)
+            .transition().duration(1000);
+    nv.utils.windowResize(chart.update);
+}
+
 // used for graphing a new file input
 function handleFileSelect(evt) {
-    $("#fileUI").hide(500); // hide the landing page
+    $("#fileUI").hide(0); // hide the landing page
     
     // read in the first file (if using multiSelect)
     var file = evt.target.files[0]; 
@@ -67,6 +93,7 @@ function handleFileSelect(evt) {
     stamp = file.name.substr(0,4) + '/' + file.name.substr(4,2) + '/' + file.name.substr(6,2);
   
     Papa.parse(file, { // parse the file as CSV using PapaParse.js
+      worker: true,
       header: true,
       dynamicTyping: true,
       comments: "#",
@@ -78,7 +105,7 @@ function handleFileSelect(evt) {
 
 // hide the landing page and plot the example data stored in exampleData.json
 function plotExampleData(){
-    $("#fileUI").hide(500); 
+    $("#fileUI").hide(0); 
     stamp = "1992/01/20"; // Ice Cube's good day
     parseData(exampleData);
 }
@@ -91,19 +118,15 @@ function parseData(data){
     var cond = [];
     var light = [];
     var head = [];
-    
-    var index; // index in object array
-    // iterate through the input object array and create an array of <values> for each dataset
-    for(index=0; index<(data.length-1);index++){ 
-        timeStamps[index] = parser.parse(stamp+ ' ' + data[index].Time);
-        temp[index] = new value(index,data[index].Temp);
-        depth[index] = new value(index,data[index].Depth);
-        cond[index] = new value(index,data[index].Cond);
-        light[index] = new value(index,data[index].Light);
-        head[index] = new value(index,data[index].Head);
-        
-    }
-    
+
+    // create an array of <values> for each dataset
+    timeStamps = data.map(function(d){return parser.parse(stamp+' '+d.Time)});
+    temp = data.map(function(d,i){return new value(i,d.Temp)});
+    depth = data.map(function(d,i){return new value(i,d.Depth)});
+    cond = data.map(function(d,i){return new value(i,d.Cond)});
+    light = data.map(function(d,i){return new value(i,d.Light)});
+    head = data.map(function(d,i){return new value(i,d.Head)});
+
     // find the minimum and maximum timestamp (can be used for domain(x))
     xMin = timeStamps[0];
     xMax = timeStamps[timeStamps.length-1];
@@ -113,11 +136,12 @@ function parseData(data){
     // arg1 = show area under curve
     // arg2 = dataset name (displayed on plot)
     // arg3 = dataset xy points (array of objects)
-    var Temperature = new dataSet(false,'Temperature',temp);
-    var Depth = new dataSet(false,"Depth (cm)",depth);
-    var Conductivity = new dataSet(false,"Conductivity",cond);
-    var Light = new dataSet(false,"Light",light);
-    var Heading = new dataSet(false,"Heading",head);
+    // arg4 = show/hide line on startup (true = hidden)
+    var Temperature = new dataSet(false,'Temperature',temp,false);
+    var Depth = new dataSet(false,"Depth",depth,false);
+    var Conductivity = new dataSet(false,"Conductivity",cond,false);
+    var Light = new dataSet(false,"Light",light,false);
+    var Heading = new dataSet(false,"Heading",head,false);
     
     // add all of the datasets to the array that will be passed to the graphing function
     plottedData[0]=Temperature;
@@ -129,7 +153,8 @@ function parseData(data){
     // plot the dataset using the lineWithFocusChart template
     nv.addGraph(function() {
         chart = nv.models.lineWithFocusChart();
-        chart.brushExtent([50,70]);
+        // default to showing points x[35],y[35] to x[65],y[65] 
+        chart.brushExtent([35,65]);
         
         //xAxis ticks correspond to HH:MM:SS from timeStamps(tick)
         chart.xAxis
@@ -147,15 +172,140 @@ function parseData(data){
         chart.y2Axis.tickFormat(d3.format(',.2f'));
         chart.useInteractiveGuideline(true);
         chart.interpolate(methods[methodNum]);
-    
+        
+        // calculate the plotted domain when the zoom level is changed
+        chart.dispatch.on('brush.update', function(b) { 
+                 min = Math.ceil(b.extent[0]);
+                 max = Math.ceil(b.extent[1]);
+                 getStats(min,max);
+        });
+        
+        // draw the chart
         d3.select('#chart svg')
             .datum(plottedData)
             .call(chart)
         nv.utils.windowResize(chart.update);
         return chart;
     });
-
 }
+
+// used to calculate statistics accross current displayed range
+function getStats(min,max){
+    var index,obj;
+    var avgTemp=0,avgDepth=0,avgCond=0,avgLight=0,avgHead=0;
+    var temp = [];
+    var depth = [];
+    var cond = [];
+    var light = [];
+    var head = [];
+    
+    // find which variables are currently displayed
+    var enabled = plottedData.map(function(d) { return !d.disabled });
+    
+    // read each category's y-data into an array
+    for(obj=0;obj<plottedData.length;obj++){
+        switch(obj){
+            case 0:
+                temp = plottedData[obj].values.map(function(d){return d.y}).slice(min,max);
+                break;
+            case 1:
+                depth = plottedData[obj].values.map(function(d){return d.y}).slice(min,max);
+                break;
+            case 2: 
+                cond = plottedData[obj].values.map(function(d){return d.y}).slice(min,max);
+                break;
+            case 3: 
+                light = plottedData[obj].values.map(function(d){return d.y}).slice(min,max);
+                break;
+            case 4: 
+                head = plottedData[obj].values.map(function(d){return d.y}).slice(min,max);
+                break;
+        }
+    }
+    
+    // only calculate/update stats when viewer is on
+    if(stats){ 
+        // clear out old statistics
+        $("#avg-data").empty();
+        // calculate & display temperature information
+        $("#avg-data").append(
+            (enabled[0] == 1 ? 
+            '<h4>Temperature</h4><p>'+
+            "min: "+ d3.min(temp).toFixed(2) + " &deg<small>C</small>"+
+            '</p><p>'+
+            "max: "+ d3.max(temp).toFixed(2) + " &deg<small>C</small>"+
+            '</p><p>'+
+            "mean: "+ d3.mean(temp).toFixed(2) + " &deg<small>C</small>"+
+            '</p><p>'+
+            "stdDev: "+ d3.deviation(temp).toFixed(2) + " &deg<small>C</small>"+
+//            '</p><p>'+
+//            'extent: ' + d3.extent(temp) + " &deg<small>C</small>"+
+            '</p>': '')
+        ); 
+        // calculate & display depth information
+        $("#avg-data").append(
+            (enabled[1] == 1 ? 
+            '<h4>Depth</h4><p>' +
+            "min: "+ d3.min(depth).toFixed(2) + " <small>cm</small>"+
+            '</p><p>'+
+            "max: "+ d3.max(depth).toFixed(2) + " <small>cm</small>"+
+            '</p><p>'+
+            "mean: "+ d3.mean(depth).toFixed(0) + " <small>cm</small>"+
+            '</p><p>' +
+            "stdDev: "+ d3.deviation(depth).toFixed(0) + " <small>cm</small>"+
+//            '</p><p>'+
+//            'extent: ' + d3.extent(depth) +" <small>cm</small>"+
+            '</p>': '')
+        );
+        // calculate & display conductivity information
+        $("#avg-data").append(
+            (enabled[2] == 1 ?
+            '<h4>Conductivity</h4><p>' +
+            "min: "+ d3.min(cond).toFixed(2) + " <small>S/m</small>"+
+            '</p><p>'+
+            "max: "+ d3.max(cond).toFixed(2) + " <small>S/m</small>"+
+            '</p><p>'+
+            "mean: "+ d3.mean(cond).toFixed(0) + " <small>S/m</small>"+
+            '</p><p>' +
+            "stdDev: "+ d3.deviation(cond).toFixed(0) + " <small>S/m</small>"+
+//            '</p><p>'+
+//            'extent: ' + d3.extent(cond) +" <small>S/m</small>"+
+            '</p>': '')
+        );
+        // calculate & display light information
+        $("#avg-data").append(
+            (enabled[3] == 1 ?
+            '<h4>Light</h4><p>' +
+            "min: "+ d3.min(light).toFixed(2) + " <small>lx</small>"+
+            '</p><p>'+
+            "max: "+ d3.max(light).toFixed(2) + " <small>lx</small>"+
+            '</p><p>' +
+            "mean: "+ d3.mean(light).toFixed(0) + " <small>lx</small>"+
+            '</p><p>'+
+            "stdDev: "+ d3.deviation(light).toFixed(0) + " <small>lx</small>"+
+//            '</p><p>'+
+//            'extent: ' + d3.extent(light) +" <small>lx</small>"+
+            '</p>': '')
+        );
+        // calculate & display heading information
+        var avgHead = d3.mean(head).toFixed(0);
+        $("#avg-data").append(
+            (enabled[4] == 1 ?
+            '<h4>Heading</h4><p>'+
+            "min: "+ d3.min(head).toFixed(2) + "&deg"+
+            '</p><p>'+
+            "max: "+ d3.max(head).toFixed(2) + "&deg"+
+            '</p><p>' +
+            "mean: "+  avgHead + '&deg ('+ degToCompass(avgHead) + ')</p><p>'+
+            "stdDev: "+ d3.deviation(head).toFixed(0) + '&deg'+
+//            '</p><p>'+
+//            'extent: ' + d3.extent(head) + '&deg'+
+            '</p>' : '')
+        );
+    }
+    return;
+}
+   
 
 // Interpolate and redisplay data based on navBar input
 function interpolateData(){
@@ -169,10 +319,18 @@ function interpolateData(){
     // update the chart data
     d3.select('#chart svg')
             .datum(plottedData)
-            .call(chart);
+            .call(chart)
+            .transition().duration(1000);
     nv.utils.windowResize(chart.update);
     
     // display the chosen method under the "Interpolate" nav bar button
     $( "#chartObj" ).empty();
     $( "#chartObj" ).append(methods[methodNum]);
+}
+
+// return a string representing compass direction for a given value in degrees
+function degToCompass(num) {
+    var val = Math.floor((num / 22.5) + 0.5);
+    var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+    return arr[(val % 16)];
 }
